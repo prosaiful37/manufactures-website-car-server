b21f1a485293222aa2a5b5bb35c0bfbccf01557f
Saiful Islam <57891501+prosaiful37@users.noreply.github.com>
2022-05-28 08:33:49 +0600

    payment specfic order api ready

--- index.js
+++ index.js
@@ -78,6 +78,12 @@ async function run() {
     })
 
 
+    app.get('/orders/:id', async(req, res) => {
+      const id = req.params.id;
+      const query  = {_id: ObjectId(id)};
+      const order =  await orderCollection.findOne(query);
+      res.send(order)
+    })
 
 
 
